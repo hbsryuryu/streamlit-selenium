0249f93c4dbaa9129357524c3f1cae014a86472b
ryuki.hamada <54260904+hbsryuryu@users.noreply.github.com>
2023-04-17 20:25:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,42 +5,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 import platform
 from webdriver_manager.core.utils import ChromeType
 
-
-
-
-
-
-
-
-
 st.title('米国株価可視化アプリ')
 
-
-
-
-if st.button('Say hello'):
-    st.write('Why hello there')
-    
+def aaaaaa():
     if (platform.system() == "Windows"):
-        chrome_service = fs.Service(executable_path=ChromeDriverManager().install())
-        # chrome_service = fs.Service(executable_path='chromedriver-107.exe')
         options = webdriver.ChromeOptions()
+        chrome_service = fs.Service(executable_path=ChromeDriverManager().install())
     else:
         options = webdriver.ChromeOptions()
-        # ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-        # options.add_argument('--user-agent=' + ua)
         options.add_argument("--headless")
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         chrome_service = fs.Service(executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-
-    
     browser = webdriver.Chrome(options=options,service=chrome_service)
-    browser.get('https://scraping-for-beginner.herokuapp.com/login_page')
-    print("test")
     browser.close()
+
+
+
+
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+    aaaaaa()
+    
     st.write('Done')
 else:
     st.write('Goodbye')
